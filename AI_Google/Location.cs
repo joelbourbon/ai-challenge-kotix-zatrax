04f27913cba1b587d7f9d21e63e0e639afc96823
joelbourbon@gmail.com <joelbourbon@gmail.com@20ef9111-c291-51aa-a3ba-024678e4ed30>
2011-12-05 21:11:15 +0000

    Commit of the project with some modifications for the matches for food destination optimization

---
+++ AI_Google/Location.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ants {
+
+	public class Location : IEquatable<Location> {
+
+		/// <summary>
+		/// Gets the row of this location.
+		/// </summary>
+		public int Row { get; private set; }
+
+		/// <summary>
+		/// Gets the column of this location.
+		/// </summary>
+		public int Col { get; private set; }
+
+		public Location (int row, int col) {
+			this.Row = row;
+			this.Col = col;
+		}
+
+    public static bool operator ==(Location a, Location b)
+    {
+      // If both are null, or both are same instance, return true.
+      if (System.Object.ReferenceEquals(a, b))
+      {
+        return true;
+      }
+
+      // If one is null, but not both, return false.
+      if (((object)a == null) || ((object)b == null))
+      {
+        return false;
+      }
+
+      // Return true if the fields match:
+      return a.Col == b.Col && a.Row == b.Row;
+    }
+
+    public static bool operator !=(Location a, Location b)
+    {
+      return !(a == b);
+    }
+
+		public override bool Equals (object obj) {
+			if (ReferenceEquals (null, obj))
+				return false;
+			if (ReferenceEquals (this, obj))
+				return true;
+			if (obj.GetType() != typeof (Location))
+				return false;
+
+			return Equals ((Location) obj);
+		}
+
+		public bool Equals (Location other) {
+			if (ReferenceEquals (null, other))
+				return false;
+			if (ReferenceEquals (this, other))
+				return true;
+
+			return other.Row == this.Row && other.Col == this.Col;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked {
+				return (this.Row * 397) ^ this.Col;
+			}
+		}
+	}
+
+	public class TeamLocation : Location, IEquatable<TeamLocation> {
+		/// <summary>
+		/// Gets the team of this ant.
+		/// </summary>
+		public int Team { get; private set; }
+
+		public TeamLocation (int row, int col, int team) : base (row, col) {
+			this.Team = team;
+		}
+
+		public bool Equals(TeamLocation other) {
+			return base.Equals (other) && other.Team == Team;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked {
+				int result = this.Col;
+				result = (result * 397) ^ this.Row;
+				result = (result * 397) ^ this.Team;
+				return result;
+			}
+		}
+	}
+	
+	public class Ant : TeamLocation, IEquatable<Ant> 
+  {
+    public Goal AntGoal = Goal.Food;
+	  public Location Destination = new Location(-1, -1);
+	  public List<Location> Path = new List<Location>();
+
+    public enum Goal
+    {
+      None,
+      Food,
+      Offense,
+      Defense,
+      Explore
+    }
+
+    public bool NoDestination()
+    {
+      if (Destination.Col == -1 && Destination.Row == -1)
+        return true;
+      return false;
+    }
+
+		public Ant (int row, int col, int team) : base (row, col, team) 
+    {
+		}
+
+    public void setGoal(Goal iGoal)
+    {
+      AntGoal = iGoal;
+    }
+
+		public bool Equals (Ant other) {
+			return base.Equals (other);
+		}
+	}
+
+	public class AntHill : TeamLocation, IEquatable<AntHill> {
+		public AntHill (int row, int col, int team) : base (row, col, team) {
+		}
+
+		public bool Equals (AntHill other) {
+			return base.Equals (other);
+		}
+	}
+}
+
---
+++ AI_Google/MyBot.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ants 
+{
+
+	class MyBot : Bot 
+  {
+
+		// DoTurn is run once per turn
+		public override void DoTurn (IGameState state)
+		{
+		  var newLocations = new List<Location>();
+		  var foodComparatorList = new List<FoodComparator>();
+		  var myFoodComparatorListSorter = new FoodComparatorListSorter();
+
+      ////
+      ////  Loop trough all my ants and foods to list all the matching possibilities
+      ////
+      foreach (Ant ant in state.MyAnts)
+        if (ant.AntGoal == Ant.Goal.Food && ant.NoDestination())  // (Ant)  If goal is food and no destination matched
+          foreach (Location foodLocation in state.FoodTiles)      // (Food) For all known food locations
+            foodComparatorList.Add(new FoodComparator(state.GetDistance(ant, foodLocation), ant, foodLocation));
+
+      // If the list is not empty, sort it and keep only the best associations
+      if(foodComparatorList.Count > 0)
+      {
+        // Sort the list with custom sorter (sort with distance)
+        foodComparatorList.Sort(myFoodComparatorListSorter);
+
+        while(foodComparatorList.Count > 0)
+        {
+          // Set the best match (now at first position in the list) to the respective ant
+          state.MyAnts.Find(ant => ant == foodComparatorList[0].Ant).Destination = foodComparatorList[0].Food;
+          FoodComparator wSelectedFoodComparator = foodComparatorList[0];
+          foreach (FoodComparator fc in foodComparatorList)
+            if (fc.Ant == wSelectedFoodComparator.Ant || fc.Food == wSelectedFoodComparator.Food)
+              foodComparatorList.Remove(fc);
+        }
+      }
+
+		  // loop through all my ants and try to give them orders
+			foreach (Ant ant in state.MyAnts) 
+      {
+				// try all the directions
+				foreach (Direction direction in Ants.Aim.Keys) 
+        {
+					// GetDestination will wrap around the map properly
+					// and give us a new location
+					Location newLoc = state.GetDestination(ant, direction);
+
+          bool isAlreadyUsed = false;
+          foreach (Location loc in newLocations)
+          {
+            if (newLoc == loc)
+              isAlreadyUsed = true;
+          }
+
+          bool doNotMove = false;
+          foreach(Location foodLoc in state.FoodTiles)
+          {
+            if(ant.Equals(foodLoc))
+              doNotMove = true;
+          }
+
+
+          // GetIsPassable returns true if the location is land
+          if (doNotMove) break;
+          
+          if (state.GetIsPassable(newLoc) && state.GetIsUnoccupied(newLoc) && !isAlreadyUsed) 
+          {
+						IssueOrder(ant, direction);
+            newLocations.Add(newLoc);
+						// stop now, don't give 1 and multiple orders
+						break;
+					}
+				}
+				
+				// check if we have time left to calculate more orders
+				if (state.TimeRemaining < 10) break;
+			}
+		}
+		
+		// The main that starts everything
+		public static void Main (string[] args) 
+    {
+			new Ants().PlayGame(new MyBot());
+		}
+
+    public class FoodComparator
+    {
+      public float Distance = 0;
+      public Location Food;
+      public Location Ant;
+
+      public FoodComparator(float iDistance, Location iAnt, Location iFood)
+      {
+        Distance = iDistance;
+        Food = iFood;
+        Ant = iAnt;
+      }
+    }
+
+    public class FoodComparatorListSorter : IComparer<FoodComparator>
+    {
+      public int Compare(FoodComparator obj1, FoodComparator obj2) { return obj1.Distance.CompareTo(obj2.Distance); }
+    }
+	}
+}
